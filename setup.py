4cc7603432f16159080f15b6c2621ce1835588fe
Reorx <novoreorx@gmail.com>
2014-05-09 17:11:41 +0800

    bump version to 0.6, fix setup problems

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = '0.5'
+__version__ = '0.6'
 
 from setuptools import setup
 
 with open('README.rst', 'r') as f:
-    desc = f.read()
+    long_desc = f.read()
 
 setup(
     name='litekv',
     version=__version__,
     author='reorx',
     url='http://github.com/reorx/litekv',
-    description=desc,
+    description="Simple kv store for Python",
+    long_description=long_desc,
     py_modules=['litekv']
 )
